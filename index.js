d3d776ae8ee9b45fdf1711b41647b8c3f7b3300d
emichou <emirchouaib96@gmail.com>
2023-08-30 14:32:11 +0100

    Add endpoint that accepts only form requests

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const morgan = require('morgan')
-const morganBody = require('morgan-body')
-const bodyParser = require('body-parser')
+const express = require("express")
+const morgan = require("morgan")
+const morganBody = require("morgan-body")
+const bodyParser = require("body-parser")
+const multer = require("multer")
 
-const notifications = require('./routes/notifications')
+const notifications = require("./routes/notifications")
 const app = express()
 app.use(bodyParser.json())
 
 morganBody(app)
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get("/", function (req, res) {
+  res.send("Hello World!")
 })
 
-app.use('/notifications', notifications)
+app.use("/notifications", notifications)
 
-app.listen(3000, '0.0.0.0', function () {
-  console.log('Listening to Port 3000')
+app.post("/upload", upload.none(), (req, res) => {
+  res.status(200).json("ok")
+})
+
+app.listen(3000, "0.0.0.0", function () {
+  console.log("Listening to Port 3000")
 })
